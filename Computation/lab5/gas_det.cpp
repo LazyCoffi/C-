93abd7c084f4c2bc23bcedcfa4e01d0fda66c7c1
邹通成 <2361637773@qq.com>
2021-12-30 16:00:39 +0800

    c++

--- Computation/lab5/gas_det.cpp
+++ Computation/lab5/gas_det.cpp
@@ -24,20 +24,20 @@ int main() {
 	
 	double res = 1;
 	for (int i = 1; i <= n; i ++) {
-		for (int j = i + 1; j <= n; j ++) {
+		for (int j = i + 1; j <= n; j ++) {//求主元
 			if (det[j][i] > det[i][i]) {
 				swap(det[j], det[i]);
-				res *= -1;
+				res *= -1;//行列式交换结果乘以-1
 			}
 		}
 
-		for (int j = i + 1; j <= n; j ++) {
+		for (int j = i + 1; j <= n; j ++) {//消元
 			for (int k = n; k >= i + 1; k --) {
 				det[j][k] = det[j][k] - det[i][k] / det[i][i] * det[j][i];
 			}
 		}
 
-		res *= det[i][i];
+		res *= det[i][i];//累计答案
 	}
 	
 	cout << res << endl;
