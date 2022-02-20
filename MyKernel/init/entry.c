a522842a6de3843960bbc9b202e6e50205297388
邹通成 <chengzou233@gmail.com>
2022-02-21 05:44:18 +0800

    console

--- MyKernel/init/entry.c
+++ MyKernel/init/entry.c
@@ -3,6 +3,6 @@
 int kern_entry(){
     console_clean_screen();
     console_print_str("Helloworld!");
-    console_print_colored_str("Color!", rc_red, rc_yellow);
+    console_print_colored_str("Color!", rc_red, rc_white);
     return 0;
 }
